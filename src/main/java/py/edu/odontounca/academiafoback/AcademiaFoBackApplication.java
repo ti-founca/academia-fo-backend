c4a28adc110d7efb27dc3d10e3d812ce5785119a
Cristhian Traversi <crist.trav@gmail.com>
2026-06-09 13:35:26 -0300

    Proyecto creado

---
+++ src/main/java/py/edu/odontounca/academiafoback/AcademiaFoBackApplication.java
@@ -0,0 +1,13 @@
+package py.edu.odontounca.academiafoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaFoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademiaFoBackApplication.class, args);
+    }
+
+}
